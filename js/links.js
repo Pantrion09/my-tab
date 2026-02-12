6260394322677fc3dc540efc99044b52db170ecf
Pantrion09 <gidaat897@gmail.com>
2026-02-12 22:50:07 +0400

    bug fix links.js

--- js/links.js
+++ js/links.js
@@ -3,16 +3,15 @@ const linkCategories = {
         { name: "ChatGPT", url: "https://chatgpt.com/" },
         { name: "Gemini", url: "https://gemini.google.com//" },
         { name: "Deepseek", url: "https://chat.deepseek.com/" },
-        { name: "Yandex Translate", url: "https://translate.yandex.ru//" }
-        { name: "GGTW", url: "https://ggntw.com/steam//" }
-        { name: "HdRezka", url: "https://hdrezka.co//" }
-        { name: "RuTracker", url: "https://rutracker.org//" }
+        { name: "Yandex Translate", url: "https://translate.yandex.ru//" },
+        { name: "GGTW", url: "https://ggntw.com/steam//" },
+        { name: "HdRezka", url: "https://hdrezka.co//" },
+        { name: "RuTracker", url: "https://rutracker.org//" },
         { name: "VSthemes", url: "https://vsthemes.org//" }
-
     ],
     social: [
         { name: "Reddit", url: "https://www.reddit.com" },
-        { name: "Github", url: "https://github.com" },
+        { name: "Github", url: "https://github.com" }
     ],
     content: [
         { name: "Spotify", url: "https://spotify.com" },
@@ -20,7 +19,7 @@ const linkCategories = {
         { name: "YouTube Music", url: "https://music.youtube.com" },
         { name: "YouTube", url: "https://www.youtube.com" },
         { name: "Pinterest", url: "https://www.pinterest.com/" },
-        { name: "Inoriginal", url: "https://inoriginal.net//" },
+        { name: "Inoriginal", url: "https://inoriginal.net//" }
     ]
 };
 function createLinks() {
